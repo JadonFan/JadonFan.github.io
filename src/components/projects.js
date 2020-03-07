8eb6c4779a7b69739513b00a4f31a3ae844b9102
Jadon Fan <j53fan@uwaterloo.ca>
2020-03-06 23:36:04 -0500

    Fixed finance icon extension

--- src/components/projects.js
+++ src/components/projects.js
@@ -11,7 +11,7 @@ class Projects extends Component {
 		Finance: {
 			description: "An Android spending tracker app to help you make better financial decisions",
 			projectFilePath: "",
-			iconPath: "images/finance_app_icon.png",
+			iconPath: "images/finance_app_icon.jpg",
 			downloadRequirements: "Requires Android Marshmallow or Later",
 			features: [],
 			techsUsed: []
